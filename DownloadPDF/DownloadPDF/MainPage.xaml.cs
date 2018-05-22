f7a06169bd2d418a7c1c3895055158233b040062
DESKTOP-KFRLMGS\SuneelPC <suneel.biyyapu@gmail.com>
2018-05-22 23:09:40 +0530

    Add project files.

---
+++ DownloadPDF/DownloadPDF/MainPage.xaml.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DownloadPDF
+{
+	public partial class MainPage : ContentPage
+	{
+		public MainPage()
+		{
+			InitializeComponent();
+		}
+
+        protected void webOnNavigating(object sender, WebNavigatingEventArgs e)
+        {
+            if (e.Url.Contains(".pdf"))
+            {
+                // Retrieving the URL
+                var pdfUrl = new Uri(e.Url);
+
+                // Open PDF URL with device browser to download
+                Device.OpenUri(pdfUrl);
+
+                // Cancel the navigation on click actions (retains in the same page.)
+                e.Cancel = true;
+            }
+        }
+    }
+}
